2962e73592d60cb38c4c7acf22c1853dff6e19c5
jennuine <jqnguyen@mix.wvu.edu>
2017-01-18 20:26:20 -0500

    implemented roboteq hw interface

--- hw_interface_plugins/hw_interface_plugin_roboteq/include/hw_interface_plugin_roboteq/hw_interface_plugin_roboteq.hpp
+++ hw_interface_plugins/hw_interface_plugin_roboteq/include/hw_interface_plugin_roboteq/hw_interface_plugin_roboteq.hpp
@@ -4,4 +4,45 @@
 #include <ros/ros.h>
 #include <pluginlib/class_list_macros.h>
 
+#include <hw_interface/shared_const_buffer.hpp>
+#include <hw_interface/base_interface.hpp>
+#include <hw_interface/base_serial_interface.hpp>
 
+#include <messages/ActuatorOut.h>
+#include <messages/encoder_data.h>
+#include <messages/GrabberFeedback.h>
+
+namespace hw_interface_plugin_roboteq {
+
+    enum controller_t {Other, Left_Drive_Roboteq, Right_Drive_Roboteq};
+
+   class roboteq_serial : public base_classes::base_serial_interface
+   {
+    public:
+        typedef boost::asio::buffers_iterator<boost::asio::streambuf::const_buffers_type> matcherIterator;
+        
+        roboteq_serial();
+        virtual ~roboteq_serial() {} //need to implement closing of the port here
+        
+    protected:
+        ros::NodeHandlePtr nh;
+        
+        messages::ActuatorOut latestActuatorCmd;
+        controller_t roboteqType;
+        
+        bool subPluginInit(ros::NodeHandlePtr nhPtr);
+        void setInterfaceOptions();
+        bool interfaceReadHandler(const long &length, int arrayStartPost);
+        bool verifyChecksum();
+        
+        virtual bool implInit() = 0;
+        virtual bool implStart() = 0;
+        virtual bool implStop() = 0;
+        virtual bool implDataHandler(const long &bufferSize, int arrayStartPos) = 0;
+        virtual void rosMsgCallback(const messages::ActuatorOut::ConstPtr &msgIn) = 0;
+        
+        std::pair<matcherIterator, bool> matchFooter(matcherIterator begin, matcherIterator end, const char *sequence);
+   };
+}
+
+#endif 
