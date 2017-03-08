405617f8ac5db67593b5a6a93d302c25b3b57160
jennuine <jqnguyen@mix.wvu.edu>
2017-03-07 19:32:00 -0500

    added arm & scoop launch and query command selection

--- hw_interface_plugins/hw_interface_plugin_roboteq/src/hw_interface_plugin_roboteq_brushed.cpp
+++ hw_interface_plugins/hw_interface_plugin_roboteq/src/hw_interface_plugin_roboteq_brushed.cpp
@@ -20,10 +20,14 @@ bool hw_interface_plugin_roboteq::brushed::implStart()
    * 4. send '# 20' to have runtime queries repeated at 20 ms delta (50 hz)
    */
   std::string initializationCmd = "";
-  if(!(ros::param::get(pluginName+"/initializationCmd", initializationCmd)))
+  if(ros::param::get(pluginName+"/initializationCmd", initializationCmd))
   {
-      ROS_WARN("Roboteq Initialization Command Unspecified, defaulting");
-      initializationCmd = "\r^ECHOF 1\r# C\r?AIC\r# 20\r";
+    initializationCmd = "?" + command_list[initializationCmd] + "\r# 20 \r";
+  }
+  else
+  {
+    ROS_WARN("Roboteq Initialization Command Unspecified, defaulting");
+    initializationCmd = "\r^ECHOF 1\r# C\r?AIC\r# 20\r";
   }
   ROS_INFO("Roboteq Init Cmd %s", initializationCmd.c_str());
   postInterfaceWriteRequest(hw_interface_support_types::shared_const_buffer(initializationCmd));
@@ -41,21 +45,6 @@ bool hw_interface_plugin_roboteq::brushed::implDataHandler()
 {
     ROS_DEBUG("%s :: Roboteq Brushed Implementation Data Handler", pluginName.c_str());
     //should check size of buffer is equal to size of msg, just in case.
-
-    // TODO: implement publishing data to rosDataPub
-    try{
-
-        hw_interface_plugin_roboteq::Analog_Input_Conversion_Info aicData;
-        aicData.channel1_mV = boost::lexical_cast<int16_t>(m_commandVal1);
-        aicData.channel2_mV = boost::lexical_cast<int16_t>(m_commandVal2);
-    }
-    catch(const std::exception &ex)
-    {
-        ROS_ERROR_EXTRA("STD Exception Caught! \r\n %s", ex.what());
-        ROS_ERROR("REGEX Container %s", receivedRegexData.c_str());
-    }
-    m_commandVal1 = "";
-    m_commandVal2 = "";
 
     return true;
 }
