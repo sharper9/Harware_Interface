03faa4c5e64bdde874156a8e9b0c53a16b6c649f
jennuine <jqnguyen@mix.wvu.edu>
2017-04-14 22:59:22 -0400

    fixes

--- hw_interface_plugins/hw_interface_plugin_roboteq/src/hw_interface_plugin_roboteq.cpp
+++ hw_interface_plugins/hw_interface_plugin_roboteq/src/hw_interface_plugin_roboteq.cpp
@@ -67,8 +67,8 @@ void hw_interface_plugin_roboteq::roboteq_serial::rosMsgCallback(const messages:
     }
     else if(roboteqType == controller_t::Arm_Roboteq)
     {
-        motorSpeedCmds += "!G 1 " + boost::lexical_cast<std::string>(msgIn->scoop_pos_cmd) + "\r";
-        motorSpeedCmds += "!G 2 " + boost::lexical_cast<std::string>(msgIn->scoop_pos_cmd) + "\r";
+        motorSpeedCmds += "!G 1 " + boost::lexical_cast<std::string>(msgIn->arm_pos_cmd) + "\r";
+        motorSpeedCmds += "!G 2 " + boost::lexical_cast<std::string>(msgIn->arm_pos_cmd) + "\r";
         postInterfaceWriteRequest(hw_interface_support_types::shared_const_buffer(motorSpeedCmds));
     }
     else if(roboteqType == controller_t::Scoop_Roboteq)
