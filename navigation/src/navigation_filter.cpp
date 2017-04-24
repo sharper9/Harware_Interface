3e46df8f600091d2607b366a50fbcc78be611420
Jared Strader <Jared.L.Strader@gmail.com>
2017-04-24 15:02:31 -0400

    minor changes and typo fixes to navigation filter

--- navigation/src/navigation_filter.cpp
+++ navigation/src/navigation_filter.cpp
@@ -34,31 +34,27 @@ void NavigationFilter::update_time()
 
 void NavigationFilter::run()
 {
-	// Predict Methods
-
-	if (turnFlag)
+	if (turnFlag) //if turing in place
 	{
 		prev_stopped = false;
 		collecting_accelerometer_data = false;
 		collected_gyro_data = false;
 
-		if (imu.new_nb1!=0)
+		if (imu.new_nb1!=0) //if imu data is available
 		{
-      // TODO: Check this - changed from filter1 to filter
-      filter.turning(imu.nb1_p,imu.nb1_q,imu.nb1_r,imu.dt1);
+			filter.turning(imu.nb1_p,imu.nb1_q,imu.nb1_r,imu.dt1); //predict state and covariance
 		}
 		else
 		{
-      // TODO: Check this - changed from filter1 to filter
-      filter.blind_turning(imu.nb1_p,imu.nb1_q,imu.nb1_r,imu.dt1);
+			filter.blind_turning(imu.nb1_p,imu.nb1_q,imu.nb1_r,imu.dt1); //predict covariance only
 		}
 
 		filter.clear_accelerometer_values();
 		imu.clear_gyro_values();
 	}
-	else if (stopFlag)
+	else if (stopFlag) //if stopped
 	{
-		if (!prev_stopped && !stop_request)
+		if (!prev_stopped && !stop_request) //this needs to be verified no obvious purpose for this statement
 		{
 			if(imu.nb1_current)
 			{
@@ -66,74 +62,73 @@ void NavigationFilter::run()
 			}
 		}
 
-
-		if (!prev_stopped&&!collecting_accelerometer_data)
+		if (!prev_stopped && !collecting_accelerometer_data) //if not previously stopped and not collecting accel data then start collecting accel data
 		{
 			collecting_accelerometer_data = true;
 		}
 
-		if ((fabs(sqrt(imu.ax*imu.ax+imu.ay*imu.ay+imu.az*imu.az)-1)< 0.05 && sqrt((imu.p)*(imu.p)+(imu.q)*(imu.q)+(imu.r)*(imu.r))<0.005) && encoders.delta_distance == 0)
+		if ((fabs(sqrt(imu.ax*imu.ax+imu.ay*imu.ay+imu.az*imu.az)-1)< 0.05 && sqrt((imu.p)*(imu.p)+(imu.q)*(imu.q)+(imu.r)*(imu.r))<0.005) && encoders.delta_distance == 0) //if no motion detected
 		{
-			if (collecting_accelerometer_data)
+			if (collecting_accelerometer_data) //if accel data is set to be started
 			{
-				if (filter.ax_values.size() > 50)
+				if (filter.ax_values.size() > 50) //check number of data points collected is enough
 				{
 					collecting_accelerometer_data = false;
-					filter.roll_pitch_G_update();
+					filter.roll_pitch_G_update(); //update attitude
 					filter.clear_accelerometer_values();
 				}
 				else
 				{
-                                        if(imu.new_nb1=0)
+					if(imu.new_nb1!=0) //check if data is available to save
 					{
 						filter.collect_accelerometer_data(imu.ax, imu.ay, imu.az);
 					}
 				}
 			}
-      // TODO: latest_nav_control_request compile error
-      if (latest_nav_control_request.runBiasRemoval) // TODO: Check this
-      {
-        if (imu.p1_values.size() > 500 && collected_gyro_data!=true)
+
+			if (0/*latest_nav_control_request.runBiasRemova*/) //run bias removal if command is sent (and no motion is detected from previous if)
+			{
+				if (imu.p1_values.size() > 500 && collected_gyro_data!=true) //if enough data points are collected and we did not already remove the bias
 				{
-					imu.calculate_gyro1_offset();
-					if(imu.good_bias1)
+					imu.calculate_gyro1_offset(); //calculate offset
+					if(imu.good_bias1) //if offset does not exceed reasonable threshold
 					{
-            collected_gyro_data = true;
+						collected_gyro_data = true;
 						imu.set_gyro1_offset();
-                                                filter.Q_phi = 2.2847e-008;
-                                                filter.Q_theta = 2.2847e-008;
-                                                filter.Q_psi = 2.2847e-008;
+						filter.Q_phi = 2.2847e-008;
+						filter.Q_theta = 2.2847e-008;
+						filter.Q_psi = 2.2847e-008;
 					}
 					else
 					{
 						imu.clear_gyro1_values();
 					}
 				}
-        else if (collected_gyro_data==true)
+		        else if (collected_gyro_data==true)
 				{
-          collected_gyro_data = true;
+					collected_gyro_data = true;
 				}
 				else
 				{
 					imu.collect_gyro1_data();
-          collected_gyro_data = false;
+					collected_gyro_data = false;
 				}
 			}
 		}
-		else
+		else //if motion is detected then predict states
 		{
 			if (imu.new_nb1!=0)
 			{
-                                filter.dead_reckoning(imu.nb1_p,imu.nb1_q,imu.nb1_r,encoders.delta_distance,imu.dt1);
+				filter.dead_reckoning(imu.nb1_p,imu.nb1_q,imu.nb1_r,encoders.delta_distance,imu.dt1);
 			}
 			else
 			{
-                                filter.blind_dead_reckoning(imu.nb1_p,imu.nb1_q,imu.nb1_r,encoders.delta_distance,imu.dt1);
+				filter.blind_dead_reckoning(imu.nb1_p,imu.nb1_q,imu.nb1_r,encoders.delta_distance,imu.dt1);
 			}
 		}
 		prev_stopped = true;
 	}
-	else
+	else //if normal drive
 	{
 		prev_stopped = false;
 		collecting_accelerometer_data = false;
@@ -142,51 +137,52 @@ void NavigationFilter::run()
 		imu.clear_gyro_values();
 		if (imu.new_nb1!=0)
 		{
-                        filter.dead_reckoning(imu.nb1_p,imu.nb1_q,imu.nb1_r,encoders.delta_distance,imu.dt1);
+			filter.dead_reckoning(imu.nb1_p,imu.nb1_q,imu.nb1_r,encoders.delta_distance,imu.dt1);
 		}
 		else
 		{
-                        filter.blind_dead_reckoning(imu.nb1_p,imu.nb1_q,imu.nb1_r,encoders.delta_distance,imu.dt1);
+			filter.blind_dead_reckoning(imu.nb1_p,imu.nb1_q,imu.nb1_r,encoders.delta_distance,imu.dt1);
 		}
-        }
+    }
 
+    //send stop request if imu data is missing
     if (imu.nb1_missed_counter>50 && imu.nb1_good && stop_request == false)
 	{
 		stop_request = true;
 		stop_time = ros::Time::now().toSec();
 	}
-    else if (stop_request == true && imu.nb1_current)
-    {
+	else if (stop_request == true && imu.nb1_current)
+	{
 		stop_request = false;
 
 		if(imu.nb1_current)
 		{
 			imu.nb1_good_prev = true;
 		}
-    }
-    else if (ros::Time::now().toSec()-stop_time>20.0 && stop_request == true)
-    {
+	}
+	else if (ros::Time::now().toSec()-stop_time>20.0 && stop_request == true)
+	{
 		stop_request = false;
-        if (imu.nb1_missed_counter>50)
-        {
-            imu.nb1_good = false;
-            imu.nb1_good_prev = false;
-        }
+		if (imu.nb1_missed_counter>50)
+		{
+			imu.nb1_good = false;
+			imu.nb1_good_prev = false;
+		}
+	}
 	else if (stop_request == false)
 	{
-            stop_time = ros::Time::now().toSec();
+		stop_time = ros::Time::now().toSec();
 	}
 
-   // TODO: latest_nav_control_request compile error
-  if (latest_nav_control_request.runBiasRemoval && collected_gyro_data)
-  {
-            nav_status_output = 1;
+	// TODO: latest_nav_control_request compile error
+	if (0/*latest_nav_control_request.runBiasRemoval*/ && collected_gyro_data) //output when bias removal is complete (if not performing bias removal output 0)
+	{
+		nav_status_output = 1;
 	}
 	else
 	{
-            nav_status_output = 0;
+		nav_status_output = 0;
 	}
-    }
 
 }
 
--- navigation/src/navigation_filter_node.cpp
+++ navigation/src/navigation_filter_node.cpp
@@ -17,43 +17,36 @@ int main(int argc, char **argv)
 
 	while(ros::ok())
 	{
-		//ROS_INFO("*-*-*-*-*-*-*-*-*-*-*-*");
-		//ROS_INFO("State = %i",navigationfilter.state);
-		navigationfilter.filter.counter++; //this should be changed to function .increment_counter();
+		//update navigation filtert time
+		navigationfilter.filter.counter++;
 		navigationfilter.update_time(); //updates dt and current_time
+
+		//filter imu data
 		navigationfilter.imu.determine_new_data();
 		navigationfilter.imu.filter_imu_values();
 		navigationfilter.imu.set_prev_counters();
+
+		//calculate distance from encoders
 		navigationfilter.encoders.adjustEncoderWrapError();
 		navigationfilter.encoders.calculateWheelDistancesFromEncoders();
-        navigationfilter.encoders.calculateDeltaDistance4Wheels(0, 0); //turnFlag, stopFlag
-//        navigationfilter.run(user_input_nav_act); // TODO: Check this
-    navigationfilter.run();
+		navigationfilter.encoders.calculateDeltaDistance4Wheels(0, 0); //turnFlag, stopFlag
 
-    // TODO: Check these below
-//		msg_NavFilterOut.new_imu1 = navigationfilter.imu.new_imu1;
-//		msg_NavFilterOut.p1 = navigationfilter.imu.p1;
-//		msg_NavFilterOut.q1 = navigationfilter.imu.q1;
-//		msg_NavFilterOut.r1 = navigationfilter.imu.r1;
-    msg_NavFilterOut.ax = navigationfilter.imu.ax1;
-    msg_NavFilterOut.ay = navigationfilter.imu.ay1;
-    msg_NavFilterOut.az = navigationfilter.imu.az1;
+		//execute navigation filter
+		navigationfilter.run();
+
+		//add navigation information to message
+		msg_NavFilterOut.roll_rate = navigationfilter.imu.p;
+		msg_NavFilterOut.pitch_rate = navigationfilter.imu.q;
+		msg_NavFilterOut.yaw_rate = navigationfilter.imu.r;
+		msg_NavFilterOut.ax = navigationfilter.imu.ax1;
+		msg_NavFilterOut.ay = navigationfilter.imu.ay1;
+		msg_NavFilterOut.az = navigationfilter.imu.az1;
+		msg_NavFilterOut.counter=navigationfilter.filter.counter;
+		msg_NavFilterOut.nav_status = navigationfilter.nav_status_output;
 		msg_NavFilterOut.p1_offset = navigationfilter.imu.p1_offset;
 		msg_NavFilterOut.q1_offset = navigationfilter.imu.q1_offset;
 		msg_NavFilterOut.r1_offset = navigationfilter.imu.r1_offset;
-//		msg_NavFilterOut.new_nb1 = navigationfilter.imu.new_nb1;
-//		msg_NavFilterOut.time1 = navigationfilter.imu.time1;
-//		msg_NavFilterOut.prev_time1 = navigationfilter.imu.prev_time1;
-    msg_NavFilterOut.dt = navigationfilter.imu.dt1;
-//		msg_NavFilterOut.nb1_current = navigationfilter.imu.nb1_current;
-//		msg_NavFilterOut.nb1_missed_counter = navigationfilter.imu.nb1_missed_counter;
-//		msg_NavFilterOut.nb1_drive_counter = navigationfilter.imu.nb1_drive_counter;
-//		msg_NavFilterOut.nb1_diff_prev = navigationfilter.imu.nb1_diff_prev;
-//		msg_NavFilterOut.nb1_good = navigationfilter.imu.nb1_good;
-//		msg_NavFilterOut.nb1_good_prev = navigationfilter.imu.nb1_good_prev;
-        msg_NavFilterOut.roll_rate = navigationfilter.imu.p;
-        msg_NavFilterOut.pitch_rate = navigationfilter.imu.q;
-        msg_NavFilterOut.yaw_rate = navigationfilter.imu.r;
+		msg_NavFilterOut.dt = navigationfilter.dt;
 		msg_NavFilterOut.x_position = navigationfilter.filter.x;
 		msg_NavFilterOut.y_position = navigationfilter.filter.y;
 		msg_NavFilterOut.roll = navigationfilter.filter.phi*180/3.1415927; 
@@ -62,15 +55,12 @@ int main(int argc, char **argv)
 		msg_NavFilterOut.human_heading = fmod(navigationfilter.filter.psi*180/3.1415927,360);
 		msg_NavFilterOut.bearing = atan2(navigationfilter.filter.y,navigationfilter.filter.x)*180/3.1415927;
 		msg_NavFilterOut.velocity = navigationfilter.encoders.delta_distance/navigationfilter.dt;
-		msg_NavFilterOut.counter=navigationfilter.filter.counter;
-		msg_NavFilterOut.nav_status = navigationfilter.nav_status_output;
-		msg_NavFilterOut.dt = navigationfilter.dt;
+		msg_NavFilterOut.delta_distance = navigationfilter.encoders.delta_distance;
 		msg_NavFilterOut.roll_init = navigationfilter.init_filter.phi*180.0/navigationfilter.PI;
-    msg_NavFilterOut.pitch_init = navigationfilter.init_filter.theta*180.0/navigationfilter.PI;
+		msg_NavFilterOut.pitch_init = navigationfilter.init_filter.theta*180.0/navigationfilter.PI;
 		msg_NavFilterOut.heading_init = navigationfilter.init_filter.psi*180.0/navigationfilter.PI;
-//		msg_NavFilterOut.stop_request = navigationfilter.stop_request;
-//		msg_NavFilterOut.stop_request_time = ros::Time::now().toSec()-navigationfilter.stop_time;
 
+		//publish navigation message
 		pub.publish(msg_NavFilterOut);
 
 		ros::spinOnce();
