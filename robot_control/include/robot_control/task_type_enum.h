1e766c03ae34d86005c93661cab4349ef40cab64
root <sharper9@mix.wvu.edu>
2018-11-24 10:12:37 -0800

    removing unecissary files

--- robot_control/include/robot_control/task_type_enum.h
+++
@@ -1,6 +0,0 @@
-#ifndef TASK_TYPE_ENUM_H
-#define TASK_TYPE_ENUM_H
-
-#define NUM_TASKS 13
-enum TASK_TYPE_T {_driveHalt_, _driveStraight_, _driveUntilLimit_, _pivot_, _scoopHalt_, _scoopSetPos_, _armHalt_, _armSetPos_, _armLowerToPitch_, _armShake_, _bucketHalt_, _bucketSetPos_, _bucketShake_};
-#endif // TASK_TYPE_ENUM_H
--- robot_control/src/pause.cpp
+++
@@ -1,6 +0,0 @@
-#include <robot_control/pause.h>
-
-bool Pause::runProc()
-{
-    return true;
-}
