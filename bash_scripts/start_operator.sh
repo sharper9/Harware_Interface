b4277dfd3df7b219e2b50e94c42e32ca7c95e0a6
root <sharper9@mix.wvu.edu>
2018-11-28 14:23:04 -0800

    Trimming more Fat

--- bash_scripts/start_operator.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-source /opt/ros/indigo/setup.bash
-source ~/rmc_ws/devel/setup.bash
-
-gnome-terminal \
-	--tab -e "bash -c 'roslaunch hw_interface hw_interface_operator_only.launch'" --title="hw_interface" \
-	--tab -e "bash -c 'sleep 2 && rosrun joy joy_node'" --title="joy"
